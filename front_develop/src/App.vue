f7727a5aea00c2989fb80fef8cd74dbdeea6d2c4
wb-kim <kim@wb-kimui-MacBookPro.local>
2023-01-31 13:40:22 +0900

    front update

---
+++ front_develop/src/App.vue
@@ -0,0 +1,113 @@
+<template>
+  <div class="v-application" :class="{ 'nav-open': $sidebar.showSidebar }">
+    <notifications></notifications>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+.vue-notifyjs.notifications {
+  .alert {
+    z-index: 10000;
+  }
+  .list-move {
+    transition: transform 0.3s, opacity 0.4s;
+  }
+  .list-item {
+    display: inline-block;
+    margin-right: 10px;
+  }
+  .list-enter-active {
+    transition: transform 0.2s ease-in, opacity 0.4s ease-in;
+  }
+  .list-leave-active {
+    transition: transform 1s ease-out, opacity 0.4s ease-out;
+  }
+
+  .list-enter {
+    opacity: 0;
+    transform: scale(1.1);
+  }
+  .list-leave-to {
+    opacity: 0;
+    transform: scale(1.2, 0.7);
+  }
+}
+.v-application {
+  font-family: 'NanumGothic', sans-serif !important;
+}
+@font-face {
+  font-family:'NanumGothic';
+  src: url('assets/fonts/NanumGothic.ttf') format('truetype');
+  font-weight: 400;
+}
+@font-face {
+  font-family:'NanumGothic';
+  src: url('assets/fonts/NanumGothicLight.ttf') format('truetype');
+  font-weight: 300;
+}
+@font-face {
+  font-family:'NanumGothic';
+  src: url('assets/fonts/NanumGothicBold.ttf') format('truetype');
+  font-weight: 700;
+}
+
+button {
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  appearance: none;
+  
+  background: var(--button-bg-color);
+  color: var(--button-color);
+  
+  margin: 0;
+  padding: 3px 6px;
+  
+  font-family: 'NanumGothic';
+  font-weight: 400;
+  text-align: center;
+  text-decoration: none;
+  
+  border: none;
+  border-radius: 4px;
+  
+  display: inline-block;
+  width: auto;
+  
+  box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06);
+  
+  cursor: pointer;
+  
+  transition: 0.5s;
+}
+
+button.success {
+  --button-bg-color: #28a745;
+  --button-hover-bg-color: #218838;
+}
+
+button.error {
+  --button-bg-color: #dc3545;
+  --button-hover-bg-color: #c82333;
+}
+
+button.warning {
+  --button-color: #212529;
+  --button-bg-color: #ffc107;
+  --button-hover-bg-color: #e0a800;
+}
+
+button:active,
+button:hover,
+button:focus {
+  background: var(--button-hover-bg-color);
+  outline: 0;
+}
+button:disabled {
+  opacity: 0.5;
+}
+</style>
---
+++ front_develop/src/components/DeleteModal.vue
@@ -0,0 +1,46 @@
+<div class="modal">
+    <div class="custom-modal-content">
+        <h3>길드 탈퇴 처리를 하시겠습니까?</h3>
+        <div class="form-group col-md-12" style="display: flex; margin-top: 20px;">
+            <div class="col-md-3" style="margin-top: 3px; text-align: right;">
+                <span style="font-size: 16px;">탈퇴 사유</span>
+            </div>
+            <div class="col-md-9">
+                <input style="width: 80%; padding-right: 10px;" id="reason">
+            </div>
+        </div>
+        <div class="form-group col-md-12" style="display: flex;">
+            <div class="col-md-3" style="margin-top: 3px; text-align: right;">
+                <span style="font-size: 16px;">승인자</span>
+            </div>
+            <div class="col-md-9">
+                <select class="adminList" id="deleteAdmin">
+                </select>
+            </div>
+        </div>
+        <div>
+            <button class="modal-delete success">확인</button>
+            <button class="modal-delete-cancel error">취소</button>
+        </div>
+    </div>
+</div>
+
+<script>
+
+</script>
+
+<style>
+.modal { 
+    position:fixed; width:100%; height:100%; background: rgba(0,0,0,0.8); top:0%; left:0%;
+}
+
+.custom_modal_content {
+    width:400px; height:300px;
+    background:#fff; border-radius:10px;
+    position:relative; top:30%; left:50%;
+    margin-top:-50px; margin-left:-200px;
+    text-align:center;
+    box-sizing:border-box; padding:74px 0;
+    line-height:23px;
+}
+</style>
