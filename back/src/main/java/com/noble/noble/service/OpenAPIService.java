0b3679f02806fb1e827b060fbd2e25b71e6346dd
wb-kim <kim@wb-kimui-MacBookPro.local>
2023-02-13 13:15:54 +0900

    cube error

--- back/src/main/java/com/noble/noble/service/OpenAPIService.java
+++ back/src/main/java/com/noble/noble/service/OpenAPIService.java
@@ -101,7 +101,7 @@ public class OpenAPIService {
                         } else {
                             newCube.put("blackCube", 0);
                             newCube.put("redCube", 0);  
-                            newCube.put("price", 0);
+                            newCube.put("price", (long)0);
                         }
                         cubeList.add(newCube);
                     }
